3bf97bf9863edf4d8bd2fde360a5ff16048bebb7
AndreaChiari <chiariandrea94@gmail.com>
2023-01-12 17:33:29 +0100

    update-v-for-dynamic -generation

--- bolzapp.js
+++ bolzapp.js
@@ -2,10 +2,22 @@ console.log('vue ok',Vue)
 
 const app = Vue.createApp({
     data(){
-        data
-        return
+         return{
+            data
+            
+         }
        
+    },
+    
+    computed: {
+        myimage:{
+            get(){
+                return 'img/avatar' + this.data.user.avatar + '.jpg'
+            }
+        },
+        
     }
 })
+
 
 app.mount('#root')
